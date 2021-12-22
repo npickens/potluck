a5eb72b8e533f30a0c10fbb252f258782c9a0ae4
Nate Pickens <nate@pickens.email>
2021-12-22 09:36:12 -0800

    Add documentation to Service class

--- lib/potluck/service.rb
+++ lib/potluck/service.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 module Potluck
+  ##
+  # Provides a Ruby interface for controlling and configuring system services.
+  #
   class Service
     SERVICE_PREFIX = 'potluck.npickens.'
 
@@ -10,6 +13,15 @@ module Potluck
 
     LAUNCHCTL_ERROR_REGEX = /^-|\t[^0]\t/.freeze
 
+    ##
+    # Creates a new instance.
+    #
+    # * +logger+ - +Logger+ instance to use for outputting info and error messages (optional). Output will
+    #   be sent to stdout and stderr if none is supplied.
+    # * +manage+ - True if the service runs locally and should be managed by this process (default: true if
+    #   launchctl is available and false otherwise).
+    # * +is_local+ - DEPRECATED. True if the service runs locally (use +manage+ instead).
+    #
     def initialize(logger: nil, manage: launchctl?, is_local: (is_local_omitted = true; nil))
       @logger = logger
       @manage = !!manage
@@ -32,38 +44,70 @@ module Potluck
       end
     end
 
+    ##
+    # Returns true if the service is managed.
+    #
     def manage?
       @manage
     end
 
+    ##
+    # Returns true if launchctl is available.
+    #
     def launchctl?
       defined?(@@launchctl) ? @@launchctl : (@@launchctl = `which launchctl 2>&1` && $? == 0)
     end
 
+    ##
+    # Checks if launchctl is available and raises an error if not.
+    #
     def ensure_launchctl!
       launchctl? || raise("Cannot manage #{self.class.to_s.split('::').last}: launchctl not found")
     end
 
+    ##
+    # Command to get the status of the service.
+    #
     def status_command
       @status_command || "launchctl list 2>&1 | grep #{SERVICE_PREFIX}#{self.class.service_name}"
     end
 
+    ##
+    # Regular expression to check the output of +#status_command+ against to determine if the service is in
+    # an error state.
+    #
     def status_error_regex
       @status_error_regex || LAUNCHCTL_ERROR_REGEX
     end
 
+    ##
+    # Command to start the service.
+    #
     def start_command
       @start_command || "launchctl bootstrap gui/#{Process.uid} #{self.class.plist_path}"
     end
 
+    ##
+    # Command to stop the service.
+    #
     def stop_command
       @stop_command || "launchctl bootout gui/#{Process.uid}/#{self.class.launchctl_name}"
     end
 
+    ##
+    # Writes the service's launchctl plist file to disk.
+    #
     def ensure_plist
       File.write(self.class.plist_path, self.class.plist)
     end
 
+    ##
+    # Returns the status of the service:
+    #
+    # * +:active+ if the service is managed and running.
+    # * +:inactive+ if the service is not managed or is not running.
+    # * +:error+ if the service is managed and is in an error state.
+    #
     def status
       return :inactive unless manage?
 
@@ -78,6 +122,9 @@ module Potluck
       end
     end
 
+    ##
+    # Starts the service if it's managed and is not active.
+    #
     def start
       return unless manage?
 
@@ -96,6 +143,9 @@ module Potluck
       log("#{self.class.pretty_name} started")
     end
 
+    ##
+    # Stops the service if it's managed and is active or in an error state.
+    #
     def stop
       return unless manage? && status != :inactive
 
@@ -107,6 +157,9 @@ module Potluck
       log("#{self.class.pretty_name} stopped")
     end
 
+    ##
+    # Restarts the service if it's managed by calling stop and then start.
+    #
     def restart
       return unless manage?
 
@@ -114,6 +167,13 @@ module Potluck
       start
     end
 
+    ##
+    # Runs a command with the default shell. Raises an error if the command exits with a non-zero status.
+    #
+    # * +command+ - Command to run.
+    # * +redirect_stderr* - True if stderr should be redirected to stdout; otherwise stderr output will not
+    #   be logged (default: true).
+    #
     def run(command, redirect_stderr: true)
       output = `#{command}#{' 2>&1' if redirect_stderr}`
       status = $?
@@ -126,6 +186,12 @@ module Potluck
       end
     end
 
+    ##
+    # Logs a message using the logger or stdout/stderr if no logger is configured.
+    #
+    # * +message+ - Message to log.
+    # * +error+ - True if the message is an error (default: false).
+    #
     def log(message, error = false)
       if @logger
         error ? @logger.error(message) : @logger.info(message)
@@ -136,6 +202,13 @@ module Potluck
 
     private
 
+    ##
+    # Calls the supplied block repeatedly until it returns false. Checks frequently at first and gradually
+    # reduces down to one-second intervals.
+    #
+    # * +timeout+ - Maximum number of seconds to wait before timing out (default: 30).
+    # * +block+ - Block to call until it returns false.
+    #
     def wait(timeout = 30, &block)
       while block.call && timeout > 0
         reduce = [[(30 - timeout.to_i) / 5.0, 0.1].max, 1].min
@@ -145,22 +218,37 @@ module Potluck
       end
     end
 
+    ##
+    # Human-friendly name of the service.
+    #
     def self.pretty_name
       @pretty_name ||= self.to_s.split('::').last
     end
 
+    ##
+    # Computer-friendly name of the service.
+    #
     def self.service_name
       @service_name ||= pretty_name.downcase
     end
 
+    ##
+    # Name for the launchctl service.
+    #
     def self.launchctl_name
       "#{SERVICE_PREFIX}#{service_name}"
     end
 
+    ##
+    # Path to the launchctl plist of the service.
+    #
     def self.plist_path
       File.join(DIR, "#{launchctl_name}.plist")
     end
 
+    ##
+    # Content of the launchctl plist.
+    #
     def self.plist(content)
       <<~EOS
         #{PLIST_XML}
@@ -180,8 +268,12 @@ module Potluck
     end
   end
 
+  ##
+  # DEPRECATED. Old name of Potluck::Service class.
+  #
   Dish = Service.clone
 
+  # :nodoc: all
   class Dish
     def self.inherited(subclass)
       warn("Potluck::Dish has been renamed to Potluck::Service. Please update #{subclass} to inherit from "\
